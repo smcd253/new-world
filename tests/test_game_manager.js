3aa783124a1079bcc89b3bc748cea60bbd7b6ac9
Spencer McDonough <shmcdono@usc.edu>
2020-05-03 02:24:25 -0700

    use resources unit tests

--- tests/test_game_manager.js
+++ tests/test_game_manager.js
@@ -53,6 +53,23 @@ describe("game_manager.new_player(name, color, ip)", function() {
     });
 });
 
+describe("game_manager.validate_player(ip)", function() {
+    it("Should return false if player is defined.", function() {
+        let game_manager = game.get_new_game_manager();
+        let name = "mocha", color = "brown", ip = "0.0.0.0";
+        game_manager.new_player(name, color, ip);
+        let result = game_manager.validate_player(ip);
+        assert.equal(result, false);
+    });
+
+    it("Should return true if player is not defined.", function() {
+        let game_manager = game.get_new_game_manager();
+        let ip = "0.0.0.0";
+        let result = game_manager.validate_player(ip);
+        assert.equal(result, true);
+    });
+});
+
 describe("game_manager.has_resources(ip, structure)", function() {
     it("Should return true if this player has enough resources to build a road.", function() {
         let game_manager = game.get_new_game_manager();
@@ -65,7 +82,7 @@ describe("game_manager.has_resources(ip, structure)", function() {
         assert.equal(result, true);
     });
 
-    it("Should return true if this player DOES NOT have enough resources to build a road.", function() {
+    it("Should return false if this player DOES NOT have enough resources to build a road.", function() {
         let game_manager = game.get_new_game_manager();
         game_manager.charge_resources = true;
         let name = "mocha", color = "brown", ip = "0.0.0.0";
@@ -87,12 +104,61 @@ describe("game_manager.has_resources(ip, structure)", function() {
         assert.equal(result, true);
     });
     
-    it("Should return true if this player DOES NOT have enough resources to build a road.", function() {
+    it("Should return false if this player DOES NOT have enough resources to build a road.", function() {
         let game_manager = game.get_new_game_manager();
         game_manager.charge_resources = true;
         let name = "mocha", color = "brown", ip = "0.0.0.0";
         game_manager.new_player(name, color, ip);
         let result = game_manager.has_resources(ip, "colony");
         assert.equal(result, false);
+    });
+
+    it("Should return true if this player DOES NOT have enough resources to build a road but charge_resources is false.", function() {
+        let game_manager = game.get_new_game_manager();
+        let name = "mocha", color = "brown", ip = "0.0.0.0";
+        game_manager.new_player(name, color, ip);
+        let result = game_manager.has_resources(ip, "colony");
+        assert.equal(result, true);
+    });
+});
+
+describe("game_manager.use_resources(ip, structure)", function() {
+    it("Should subtract 1 brick and 1 wood from the player's hand if we call this function on 'road.'", function() {
+        let game_manager = game.get_new_game_manager();
+        game_manager.charge_resources = true;
+        let name = "mocha", color = "brown", ip = "0.0.0.0";
+        game_manager.new_player(name, color, ip);
+        game_manager.players[ip].hand['wood'] = 1;
+        game_manager.players[ip].hand['brick'] = 1;
+        game_manager.use_resources(ip, "road");
+        assert.equal(game_manager.players[ip].hand['wood'], 0);
+        assert.equal(game_manager.players[ip].hand['brick'], 0);
+    });
+
+    it("Should subtract 1 brick, wood, wheat, and sheep from the player's hand if we call this function on 'colony.'", function() {
+        let game_manager = game.get_new_game_manager();
+        game_manager.charge_resources = true;
+        let name = "mocha", color = "brown", ip = "0.0.0.0";
+        game_manager.new_player(name, color, ip);
+        game_manager.players[ip].hand['wood'] = 1;
+        game_manager.players[ip].hand['brick'] = 1;
+        game_manager.players[ip].hand['wheat'] = 1;
+        game_manager.players[ip].hand['sheep'] = 1;
+        game_manager.use_resources(ip, "colony");
+        assert.equal(game_manager.players[ip].hand['wood'], 0);
+        assert.equal(game_manager.players[ip].hand['brick'], 0);
+        assert.equal(game_manager.players[ip].hand['wheat'], 0);
+        assert.equal(game_manager.players[ip].hand['sheep'], 0);
+    });
+
+    it("Should NOT subtract anything if charge_resources is false.", function() {
+        let game_manager = game.get_new_game_manager();
+        let name = "mocha", color = "brown", ip = "0.0.0.0";
+        game_manager.new_player(name, color, ip);
+        game_manager.use_resources(ip, "colony");
+        assert.equal(game_manager.players[ip].hand['wood'], 0);
+        assert.equal(game_manager.players[ip].hand['brick'], 0);
+        assert.equal(game_manager.players[ip].hand['wheat'], 0);
+        assert.equal(game_manager.players[ip].hand['sheep'], 0);
     });
 });
